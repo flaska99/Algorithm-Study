856ef5bb4a7de90bb38fbb188ef545d1e0ecd42f
flaska99 <tmdalsehrwhs@naver.com>
2025-03-25 15:58:31 +0900

    circular queue

--- data structure/Circular Queue/원형큐.py
+++ data structure/Circular Queue/원형큐.py
@@ -11,7 +11,7 @@ class CircularQueue:
     def enQueue(self, value):
         if self.q[self.rear] is None:
             self.q[self.rear] = value   # rear 포인터에 입력받은 인자를 넣는다.
-            self.rear = (self.rear + 1) % self.len  # 전체길이로 나머지연산을 하여 포인터가 전체기리를 넘지 못하게 이동시킨다.
+            self.rear = (self.rear + 1) % self.len  # 전체길이로 나머지연산을 하여 포인터가 전체거리를 넘지 못하게 이동시킨다.
             return True
         else:
             return False
@@ -19,7 +19,7 @@ class CircularQueue:
     # 추출 메소드
     def deQueue(self):
         if self.q[self.front] is not None:
-            result = self.q[self.front]     # result 변수에 배열의 첫번째 요소를 답는다.
+            result = self.q[self.front]     # result 변수에 배열의 첫번째 요소를 담는다.
             self.q[self.front] = None   # 배열의 첫번쨰요소를 삭제
             self.front = (self.front + 1) % self.len
             return result     # result 변수 리턴
